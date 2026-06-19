ac90c1760099cd0c4d022ea8ab88ca4cf93eaf9b
silversPuppet <silvers.puppet@gmail.com>
2026-06-19 11:46:18 +0200

    unicafe 1.7-1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,15 +5,45 @@ const Button = ({label, onClick}) => {
     <button onClick={onClick}>{label}</button>
   )
 }
-const Statistics = ({good, neutral, bad}) => {
-  return(
+const Statistics = (props) => {
+  const values = [props.good, props.neutral, props.bad]
+  const sum = (values) => {
+    let sum = 0
+    for(let i = 0; i < values.length; i++){
+      sum += values[i];
+    }
+    return sum
+  }
+
+  const average = () => {
+    return sum([props.good, 0, -props.bad]) / sum(values)
+  }
+
+  const percentFirstValue = () => {
+      return (values[0] / sum(values)) * 100
+  }
+  if(sum(values) == 0)
+  {
+    return(
     <div>
       <h1>statistics</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
+      <p>No feedback given.</p>
     </div>
   )
+  }
+  else{
+    return(
+      <div>
+        <h1>statistics</h1>
+        <p>good: {props.good}</p>
+        <p>neutral: {props.neutral}</p>
+        <p>bad: {props.bad}</p>
+        <p>all: {sum(values)}</p>
+        <p>average: {average()} </p>
+        <p>positive: {percentFirstValue()} %</p>
+      </div>
+    )
+  }
 }
 
 const App = () => {
