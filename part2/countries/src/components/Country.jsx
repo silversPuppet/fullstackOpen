6f7ba3fb1532479400395239aaeff4fe76fb743b
silversPuppet <silvers.puppet@gmail.com>
2026-07-30 17:56:52 +0200

    countries 2.20

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,27 +1,47 @@
-const Country = ({ country, isDetailed, showDetail }) => {
-  if(isDetailed){
-    return(
-       <div>
-        <h1>{country.name.common}</h1>
-        <div>
-            <p>Capital: {country.capital}</p>
-            <p>Area: {country.area}</p>
-            <h2>Languages</h2>
-            <ul>
-                {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
-            </ul>
-            <img src={country.flags.png} alt={country.flags.alt} />
-        </div>
+import countryService from "../services/countries"
+import { useState, useEffect } from 'react'
 
-    </div>
-    )
-  }else{
-  return (
-      <li>
-        {country.name.common} 
-        <button onClick={showDetail}>Show</button>
-      </li>
-    )
+const Country = ({ country, isDetailed, showDetail  }) => {
+  const [weather, setWeather] = useState({})
+
+  useEffect(() => {
+    if (isDetailed) {
+      countryService.getWeather(country.capital)
+        .then(weatherData => setWeather(weatherData))
+        .catch(error => console.error(error))
+        console.log(country.capital)
+    }
+  }, [isDetailed, country.capital])
+
+  if(!isDetailed){
+    return (
+        <li>
+          {country.name.common} 
+          <button onClick={showDetail}>Show</button>
+        </li>
+      )
   }
+
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <div>
+        <p>Capital: {country.capital}</p>
+        <p>Area: {country.area}</p>
+        <h2>Languages</h2>
+        <ul>
+          {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
+        </ul>
+        <img src={country.flags.png} alt={country.flags.alt} />
+        <div>
+          <h2>Weather in {country.capital}</h2>
+          <p>Temperature: {weather.main.temp} °C</p>
+          <img src={`https://openweathermap.org/payload/api/media/file/${weather.weather[0].icon}.png`} /> 
+          <p>Wind: {weather.wind.speed} km/h</p> 
+        </div>
+      </div>
+    </div>
+  )
+
 }
 export default Country
