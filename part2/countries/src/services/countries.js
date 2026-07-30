245d8f46f72b09bcc012f515b1912de8e0514c29
silversPuppet <silvers.puppet@gmail.com>
2026-07-30 17:56:52 +0200

    countries 2.20

--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/'
+const api_key = import.meta.env.VITE_SOME_KEY
 
 const getAll = () => {
   const request = axios.get(`${baseUrl}/all`)
@@ -12,4 +13,27 @@ const getCountry = (name) => {
     return request.then(response => response.data)
 }
 
-export default{getAll, getCountry }
+const getWeather = (capitalName) => {
+    console.log(capitalName)
+    return axios
+    .get(`https://api.openweathermap.org/geo/1.0/direct?q=${capitalName}&limit=1&appid=${api_key}`)
+    .then(geoResponse => {
+        const lat = geoResponse.data[0].lat
+        const lon = geoResponse.data[0].lon 
+        console.log(lat, lon)
+
+        return(
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${api_key}`)
+        )
+    })
+    .then(weatherResponse => {
+        console.log(weatherResponse.data)
+        return weatherResponse.data
+    })
+    .catch(error => {
+      console.error('Weather fetch failed:', error.response?.status, error.response?.data);
+      throw error;
+    });
+}
+
+export default{getAll, getCountry, getWeather }
